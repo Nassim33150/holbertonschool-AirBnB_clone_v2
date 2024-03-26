e03d5657ccfacb8db93a904b35dae934e6f89968
root <root@LAPTOP-2LV4F0PM>
2024-03-26 13:42:40 +0100

    model state  mapped to states tables in DB

--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", backref="state")
+
+    def cities(self):
+        """ Retrieve all cities from storage """
+        all_cities = storage.all(City)
+        state_cities = [city for city in all_cities.values()
+                        if city.state_id == self.id]
+
+        return state_cities
