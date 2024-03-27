b7da0deb99cdc270f4061449c62a25c1c7d67303
root <root@LAPTOP-2LV4F0PM>
2024-03-27 12:18:02 +0100

    review table

--- models/review.py
+++ models/review.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.engine.db_storage import DBStorage
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.user import User
+from models.city import City
+from models.place import Place
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ The review class, contains user ID and place ID """
+    __tablename__ = "reviews"
+
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey(places.id), nullable=False)
+    user_id = Column(String(60), ForeignKey(users.id), nullable=False)
