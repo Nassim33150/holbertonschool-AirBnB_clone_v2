d8b9941034231c0fab2d161d6f121491995a7c42
Mamuche <marionlaroche17@gmail.com>
2024-03-29 15:42:47 +0100

    add tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -1,5 +1,11 @@
+#!/usr/bin/python3
+"""Defines unnittests for models/engine/db_storage.py."""
+import pep8
+import models
+import MySQLdb
 import unittest
-from models.base_model import BaseModel, Base
+from os import getenv
+from models.base_model import Base
 from models.user import User
 from models.state import State
 from models.city import City
@@ -7,252 +13,176 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 from models.engine.db_storage import DBStorage
-from os import getenv
+from models.engine.file_storage import FileStorage
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.engine.base import Engine
 
 
 class TestDBStorage(unittest.TestCase):
-    def setUp(self):
-        """Set up the test environment"""
-        self.db = DBStorage()
+    """Unittests for testing the DBStorage class."""
 
-    def tearDown(self):
-        """Clean up the test environment"""
-        self.db.delete()
+    @classmethod
+    def setUpClass(cls):
+        """DBStorage testing setup.
 
+        Instantiate new DBStorage.
+        Fill DBStorage test session with instances of all classes.
+        """
+        if type(models.storage) == DBStorage:
+            cls.storage = DBStorage()
+            Base.metadata.create_all(cls.storage._DBStorage__engine)
+            Session = sessionmaker(bind=cls.storage._DBStorage__engine)
+            cls.storage._DBStorage__session = Session()
+            cls.state = State(name="California")
+            cls.storage._DBStorage__session.add(cls.state)
+            cls.city = City(name="San_Jose", state_id=cls.state.id)
+            cls.storage._DBStorage__session.add(cls.city)
+            cls.user = User(email="poppy@holberton.com", password="betty")
+            cls.storage._DBStorage__session.add(cls.user)
+            cls.place = Place(city_id=cls.city.id, user_id=cls.user.id,
+                              name="School")
+            cls.storage._DBStorage__session.add(cls.place)
+            cls.amenity = Amenity(name="Wifi")
+            cls.storage._DBStorage__session.add(cls.amenity)
+            cls.review = Review(place_id=cls.place.id, user_id=cls.user.id,
+                                text="stellar")
+            cls.storage._DBStorage__session.add(cls.review)
+            cls.storage._DBStorage__session.commit()
+
+    @classmethod
+    def tearDownClass(cls):
+        """DBStorage testing teardown.
+
+        Delete all instantiated test classes.
+        Clear DBStorage session.
+        """
+        if type(models.storage) == DBStorage:
+            cls.storage._DBStorage__session.delete(cls.state)
+            cls.storage._DBStorage__session.delete(cls.city)
+            cls.storage._DBStorage__session.delete(cls.user)
+            cls.storage._DBStorage__session.delete(cls.amenity)
+            cls.storage._DBStorage__session.commit()
+            del cls.state
+            del cls.city
+            del cls.user
+            del cls.place
+            del cls.amenity
+            del cls.review
+            cls.storage._DBStorage__session.close()
+            del cls.storage
+
+    def test_pep8(self):
+        """Test pep8 styling."""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
+
+    def test_docstrings(self):
+        """Check for docstrings."""
+        self.assertIsNotNone(DBStorage.__doc__)
+        self.assertIsNotNone(DBStorage.__init__.__doc__)
+        self.assertIsNotNone(DBStorage.all.__doc__)
+        self.assertIsNotNone(DBStorage.new.__doc__)
+        self.assertIsNotNone(DBStorage.save.__doc__)
+        self.assertIsNotNone(DBStorage.delete.__doc__)
+        self.assertIsNotNone(DBStorage.reload.__doc__)
+
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
+    def test_attributes(self):
+        """Check for attributes."""
+        self.assertTrue(isinstance(self.storage._DBStorage__engine, Engine))
+        self.assertTrue(isinstance(self.storage._DBStorage__session, Session))
+
+    def test_methods(self):
+        """Check for methods."""
+        self.assertTrue(hasattr(DBStorage, "__init__"))
+        self.assertTrue(hasattr(DBStorage, "all"))
+        self.assertTrue(hasattr(DBStorage, "new"))
+        self.assertTrue(hasattr(DBStorage, "save"))
+        self.assertTrue(hasattr(DBStorage, "delete"))
+        self.assertTrue(hasattr(DBStorage, "reload"))
+
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
+    def test_init(self):
+        """Test initialization."""
+        self.assertTrue(isinstance(self.storage, DBStorage))
+
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
     def test_all(self):
-        """Test the all method"""
-        # Create some objects
-        user = User()
-        state = State()
-        city = City()
-        amenity = Amenity()
-        place = Place()
-        review = Review()
+        """Test default all method."""
+        obj = self.storage.all()
+        self.assertEqual(type(obj), dict)
+        self.assertEqual(len(obj), 6)
 
-        # Add objects to the database
-        self.db.new(user)
-        self.db.new(state)
-        self.db.new(city)
-        self.db.new(amenity)
-        self.db.new(place)
-        self.db.new(review)
-        self.db.save()
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
+    def test_all_cls(self):
+        """Test all method with specified cls."""
+        obj = self.storage.all(State)
+        self.assertEqual(type(obj), dict)
+        self.assertEqual(len(obj), 1)
+        self.assertEqual(self.state, list(obj.values())[0])
 
-        # Retrieve all objects from the database
-        objects = self.db.all()
-
-        # Check if all objects are retrieved
-        self.assertIn(user, objects.values())
-        self.assertIn(state, objects.values())
-        self.assertIn(city, objects.values())
-        self.assertIn(amenity, objects.values())
-        self.assertIn(place, objects.values())
-        self.assertIn(review, objects.values())
-
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
     def test_new(self):
-        """Test the new method"""
-        # Create an object
-        user = User()
+        """Test new method."""
+        st = State(name="Washington")
+        self.storage.new(st)
+        store = list(self.storage._DBStorage__session.new)
+        self.assertIn(st, store)
 
-        # Add the object to the database
-        self.db.new(user)
-        self.db.save()
-
-        # Retrieve the object from the database
-        objects = self.db.all()
-
-        # Check if the object is retrieved
-        self.assertIn(user, objects.values())
-
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
     def test_save(self):
-        """Test the save method"""
-        # Create an object
-        user = User()
+        """Test save method."""
+        st = State(name="Virginia")
+        self.storage._DBStorage__session.add(st)
+        self.storage.save()
+        db = MySQLdb.connect(user="hbnb_test",
+                             passwd="hbnb_test_pwd",
+                             db="hbnb_test_db")
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states WHERE BINARY name = 'Virginia'")
+        query = cursor.fetchall()
+        self.assertEqual(1, len(query))
+        self.assertEqual(st.id, query[0][0])
+        cursor.close()
 
-        # Add the object to the database
-        self.db.new(user)
-
-        # Save the changes
-        self.db.save()
-
-        # Retrieve the object from the database
-        objects = self.db.all()
-
-        # Check if the object is retrieved
-        self.assertIn(user, objects.values())
-
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
     def test_delete(self):
-        """Test the delete method"""
-        # Create an object
-        user = User()
+        """Test delete method."""
+        st = State(name="New_York")
+        self.storage._DBStorage__session.add(st)
+        self.storage._DBStorage__session.commit()
+        self.storage.delete(st)
+        self.assertIn(st, list(self.storage._DBStorage__session.deleted))
 
-        # Add the object to the database
-        self.db.new(user)
-        self.db.save()
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
+    def test_delete_none(self):
+        """Test delete method with None."""
+        try:
+            self.storage.delete(None)
+        except Exception:
+            self.fail
 
-        # Delete the object from the database
-        self.db.delete(user)
-        self.db.save()
+    @unittest.skipIf(type(models.storage) == FileStorage,
+                     "Testing FileStorage")
+    def test_reload(self):
+        """Test reload method."""
+        og_session = self.storage._DBStorage__session
+        self.storage.reload()
+        self.assertIsInstance(self.storage._DBStorage__session, Session)
+        self.assertNotEqual(og_session, self.storage._DBStorage__session)
+        self.storage._DBStorage__session.close()
+        self.storage._DBStorage__session = og_session
 
-        # Retrieve the object from the database
-        objects = self.db.all()
 
-        # Check if the object is deleted
-        self.assertNotIn(user, objects.values())
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
-# ...
-
-class TestDBStorage(unittest.TestCase):
-    # ...
-
-    def test_all_with_class(self):
-        """Test the all method with a specific class"""
-        # Create some objects
-        user = User()
-        state = State()
-        city = City()
-        amenity = Amenity()
-        place = Place()
-        review = Review()
-
-        # Add objects to the database
-        self.db.new(user)
-        self.db.new(state)
-        self.db.new(city)
-        self.db.new(amenity)
-        self.db.new(place)
-        self.db.new(review)
-        self.db.save()
-
-        # Retrieve objects of a specific class from the database
-        objects = self.db.all(cls='User')
-
-        # Check if objects of the specified class are retrieved
-        self.assertIn(user, objects.values())
-        self.assertNotIn(state, objects.values())
-        self.assertNotIn(city, objects.values())
-        self.assertNotIn(amenity, objects.values())
-        self.assertNotIn(place, objects.values())
-        self.assertNotIn(review, objects.values())
-
-    def test_new_multiple_objects(self):
-        """Test the new method with multiple objects"""
-        # Create multiple objects
-        user1 = User()
-        user2 = User()
-        user3 = User()
-
-        # Add the objects to the database
-        self.db.new(user1)
-        self.db.new(user2)
-        self.db.new(user3)
-        self.db.save()
-
-        # Retrieve all objects from the database
-        objects = self.db.all()
-
-        # Check if all objects are retrieved
-        self.assertIn(user1, objects.values())
-        self.assertIn(user2, objects.values())
-        self.assertIn(user3, objects.values())
-
-    # ...
-
-if __name__ == '__main__':
-    unittest.main()
-# ...
-
-class TestDBStorage(unittest.TestCase):
-    # ...
-
-    def test_all_with_class(self):
-        """Test the all method with a specific class"""
-        # Create some objects
-        user = User()
-        state = State()
-        city = City()
-        amenity = Amenity()
-        place = Place()
-        review = Review()
-
-        # Add objects to the database
-        self.db.new(user)
-        self.db.new(state)
-        self.db.new(city)
-        self.db.new(amenity)
-        self.db.new(place)
-        self.db.new(review)
-        self.db.save()
-
-        # Retrieve objects of a specific class from the database
-        objects = self.db.all(cls='User')
-
-        # Check if objects of the specified class are retrieved
-        self.assertIn(user, objects.values())
-        self.assertNotIn(state, objects.values())
-        self.assertNotIn(city, objects.values())
-        self.assertNotIn(amenity, objects.values())
-        self.assertNotIn(place, objects.values())
-        self.assertNotIn(review, objects.values())
-
-    def test_new_multiple_objects(self):
-        """Test the new method with multiple objects"""
-        # Create multiple objects
-        user1 = User()
-        user2 = User()
-        user3 = User()
-
-        # Add the objects to the database
-        self.db.new(user1)
-        self.db.new(user2)
-        self.db.new(user3)
-        self.db.save()
-
-        # Retrieve all objects from the database
-        objects = self.db.all()
-
-        # Check if all objects are retrieved
-        self.assertIn(user1, objects.values())
-        self.assertIn(user2, objects.values())
-        self.assertIn(user3, objects.values())
-
-    def test_save(self):
-        """Test the save method"""
-        # Create an object
-        user = User()
-
-        # Add the object to the database
-        self.db.new(user)
-
-        # Save the changes
-        self.db.save()
-
-        # Retrieve the object from the database
-        objects = self.db.all()
-
-        # Check if the object is retrieved
-        self.assertIn(user, objects.values())
-
-    def test_delete(self):
-        """Test the delete method"""
-        # Create an object
-        user = User()
-
-        # Add the object to the database
-        self.db.new(user)
-        self.db.save()
-
-        # Delete the object from the database
-        self.db.delete(user)
-        self.db.save()
-
-        # Retrieve the object from the database
-        objects = self.db.all()
-
-        # Check if the object is deleted
-        self.assertNotIn(user, objects.values())
-
-# ...
