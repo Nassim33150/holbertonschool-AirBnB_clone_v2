a51e2220227444d33f66b7f4b5bf9b69fdca88cf
MadMartigan3 <gabrielmilan3@hotmail.com>
2024-03-29 11:53:28 +0100

    Correction Setup mysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,6 @@
 -- If the the database hbnb_dev_db does not exist, create the database
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
--- If the user does not exist, create the user and grant all privileges
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd'; 
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schéma.* TO 'hbnb_dev'@'localhost';
