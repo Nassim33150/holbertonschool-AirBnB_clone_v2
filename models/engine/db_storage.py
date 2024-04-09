4f4579ac6e17f7fb9e4894bc026daead7c15f868
root <root@LAPTOP-2LV4F0PM>
2024-04-09 12:39:44 +0200

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -66,4 +66,8 @@ class DBStorage:
     def reload(self):
         Base.metadata.create_all(self.__engine)
         self.__session = scoped_session(sessionmaker(bind=self.__engine, 
-                                                         expire_on_commit=False))
+                                       expire_on_commit=False))
+
+    def close(self):
+        """Close the session"""
+        self.__session.remove()
