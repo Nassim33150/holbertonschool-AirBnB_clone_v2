195b38148901e760e1ae87ed9c7060bb622e6c3b
root <root@LAPTOP-2LV4F0PM>
2024-04-10 14:39:30 +0200

    states list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+""" This modules import Flask class """
 from flask import Flask, render_template
 from models import storage
 
-app = Flask(__name__)
+if __name__ == '__main__':
+    app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    sql = "SELECT * FROM states ORDER BY name ASC"
     states = storage.all('State')
     sorted_states = sorted(states.values(), key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
