b37e11f874a8aa48b38558f512dabd48d43a91f7
root <root@LAPTOP-2LV4F0PM>
2024-04-10 15:21:09 +0200

    states_list

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,25 +4,20 @@ from flask import Flask, render_template
 from models import storage
 
 """Starts a Flask web application"""
-if __name__ == '__main__':
-    app = Flask(__name__)
+app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-""" This function returns a template with all states """
-
-
 def states_list():
+    """ This function returns a template with all states """
     states = storage.all('State')
     sorted_states = sorted(states.values(), key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
 
 
 @app.teardown_appcontext
-""" This function closes the session """
-
-
 def teardown_db(exception):
+    """ This function closes the session """
     storage.close()
 
 
