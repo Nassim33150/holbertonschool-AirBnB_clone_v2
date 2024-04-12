c1064729d265441f86b704bba2c90b31a7fe0c38
root <root@LAPTOP-2LV4F0PM>
2024-04-12 11:21:56 +0200

    update python is cool

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,6 +21,7 @@ def c_is_fun(text):
     return "C " + text.replace("_", " ")
 
 
+@app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_is_cool(text="is cool"):
     return "Python " + text.replace("_", " ")
